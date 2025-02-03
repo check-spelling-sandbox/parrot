8c60db9315747e55e5addf99b62f3d92b5553032
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-02-03 15:14:56 -0500

    spelling: needs
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- include/parrot/debugger.h
+++ include/parrot/debugger.h
@@ -60,7 +60,7 @@ typedef struct PDB_condition {
     unsigned short          type;
     unsigned char           reg;
     unsigned char           dummy;      /* For alignment XXX ??  */
-    void                    *value;     /* What neeeds to be aligned with what? */
+    void                    *value;     /* What needs to be aligned with what? */
     PDB_condition_ptr       next;
 } PDB_condition_t;
 
