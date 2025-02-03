94b18130fe6d5db252598b491698edd5dc07858c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-02-03 11:52:09 -0500

    spelling: dependencies
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- include/parrot/packfile.h
+++ include/parrot/packfile.h
@@ -219,7 +219,7 @@ struct PackFile_ByteCode {
     op_func_t                    *op_func_table;   /* opcode dispatch table */
     op_func_t                    *save_func_table; /* for when we hijack op_func_table */
     op_info_t                   **op_info_table;
-    size_t                        n_libdeps;       /* number of library dependancies */
+    size_t                        n_libdeps;       /* number of library dependencies */
     STRING                      **libdeps;         /* names of prerequisite libraries */
 };
 
